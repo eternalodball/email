84e39aad7619008ba43e51d1b9a5a4b748de14eb
Cursed Soul <84136173+eternalodball@users.noreply.github.com>
2022-02-15 08:36:13 +0000

    Add files via upload

---
+++ check.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "https://mailcheck.p.rapidapi.com/"
+print()
+YOUR_DOMAIN = input('[+] ' 'Enter your email or domain to check: ')
+print()
+#YOUR_API = input('[+] ' 'Enter your API KEY: ')
+#print()
+print('[+] Checking if domain sould be blocked')
+
+querystring = {"domain": YOUR_DOMAIN}
+
+headers = {
+    'x-rapidapi-host': "mailcheck.p.rapidapi.com",
+    'x-rapidapi-key': "9b1b4e72d6mshe601fb8d92e915ap14ba13jsnbf615ace9699"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+temp = response.json()
+
+print()
+print(temp["block"])
+print()
