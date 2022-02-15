55e21db4d11d8fba8076da41c68d4f891fb1883e
Cursed Soul <84136173+eternalodball@users.noreply.github.com>
2022-02-15 08:37:33 +0000

    Update check.py

--- check.py
+++ check.py
@@ -4,15 +4,15 @@ url = "https://mailcheck.p.rapidapi.com/"
 print()
 YOUR_DOMAIN = input('[+] ' 'Enter your email or domain to check: ')
 print()
-#YOUR_API = input('[+] ' 'Enter your API KEY: ')
-#print()
+YOUR_API = input('[+] ' 'Enter your API KEY: ')
+print()
 print('[+] Checking if domain sould be blocked')
 
 querystring = {"domain": YOUR_DOMAIN}
 
 headers = {
     'x-rapidapi-host': "mailcheck.p.rapidapi.com",
-    'x-rapidapi-key': "9b1b4e72d6mshe601fb8d92e915ap14ba13jsnbf615ace9699"
+    'x-rapidapi-key': YOUR_API
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
